a0ef9468c35d87426b64b38509399238c2e8552d
skander nafti <skandernafti.pro@gmail.com>
2026-05-03 18:54:03 +0100

    fix: use IDENTITY(o.recruiter) instead of o.recruiterId in DQL queries

--- migrations/Version20260503174920.php
+++ migrations/Version20260503174920.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20260411172039 extends AbstractMigration
+final class Version20260503174920 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,20 +20,22 @@ final class Version20260411172039 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE activities (id_activity INT AUTO_INCREMENT NOT NULL, activity_date DATE NOT NULL, description LONGTEXT DEFAULT NULL, hours_worked NUMERIC(5, 2) DEFAULT NULL, employee_id INT NOT NULL, project_id INT DEFAULT NULL, INDEX IDX_B5F1AFE58C03F15C (employee_id), INDEX IDX_B5F1AFE5166D1F9C (project_id), PRIMARY KEY(id_activity)) DEFAULT CHARACTER SET utf8mb4');
-        $this->addSql('CREATE TABLE applications (id INT AUTO_INCREMENT NOT NULL, cv_file_path VARCHAR(500) DEFAULT NULL, motivation_letter LONGTEXT DEFAULT NULL, status VARCHAR(50) DEFAULT \'Nouvelle\' NOT NULL, application_date DATE DEFAULT NULL, score DOUBLE PRECISION DEFAULT 0 NOT NULL, notes LONGTEXT DEFAULT NULL, interviewer VARCHAR(150) DEFAULT NULL, interview_date DATE DEFAULT NULL, interview_result VARCHAR(100) DEFAULT NULL, recruiter_response LONGTEXT DEFAULT NULL, response_date DATE DEFAULT NULL, user_id INT NOT NULL, offer_id INT NOT NULL, INDEX IDX_F7C966F0A76ED395 (user_id), INDEX IDX_F7C966F053C674EE (offer_id), UNIQUE INDEX unique_user_offer (user_id, offer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE activities (id_activity INT AUTO_INCREMENT NOT NULL, activity_date DATE NOT NULL, description LONGTEXT DEFAULT NULL, hours_worked NUMERIC(5, 2) DEFAULT NULL, user_report LONGTEXT DEFAULT NULL, report_status VARCHAR(20) DEFAULT \'PENDING\' NOT NULL, expected_deadline DATETIME DEFAULT NULL, is_late_submission TINYINT(1) DEFAULT 0 NOT NULL, delay_in_hours INT DEFAULT 0 NOT NULL, admin_feedback LONGTEXT DEFAULT NULL, revision_count INT DEFAULT 0 NOT NULL, employee_id INT NOT NULL, project_id INT DEFAULT NULL, INDEX IDX_B5F1AFE58C03F15C (employee_id), INDEX IDX_B5F1AFE5166D1F9C (project_id), PRIMARY KEY(id_activity)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE applications (id INT AUTO_INCREMENT NOT NULL, cv_file_path VARCHAR(500) DEFAULT NULL, motivation_letter LONGTEXT DEFAULT NULL, status VARCHAR(50) DEFAULT \'Nouvelle\' NOT NULL, application_date DATE DEFAULT NULL, score DOUBLE PRECISION DEFAULT 0 NOT NULL, notes LONGTEXT DEFAULT NULL, interviewer VARCHAR(150) DEFAULT NULL, interview_date DATE DEFAULT NULL, interview_result VARCHAR(100) DEFAULT NULL, recruiter_response LONGTEXT DEFAULT NULL, response_date DATE DEFAULT NULL, workflow JSON DEFAULT NULL, user_id INT NOT NULL, offer_id INT NOT NULL, INDEX IDX_F7C966F0A76ED395 (user_id), INDEX IDX_F7C966F053C674EE (offer_id), UNIQUE INDEX unique_user_offer (user_id, offer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE bookmarks (id INT AUTO_INCREMENT NOT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, user_id INT NOT NULL, offer_id INT NOT NULL, INDEX IDX_78D2C140A76ED395 (user_id), INDEX IDX_78D2C14053C674EE (offer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE choix (id INT AUTO_INCREMENT NOT NULL, texte VARCHAR(500) NOT NULL, is_correct TINYINT(1) DEFAULT 0 NOT NULL, question_id INT NOT NULL, INDEX IDX_4F4880911E27F6BF (question_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE event (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, event_type ENUM(\'MEETUP\',\'CONFERENCE\',\'WORKSHOP\',\'WEBINAR\'), event_date DATETIME NOT NULL, end_date DATETIME DEFAULT NULL, location VARCHAR(500) DEFAULT NULL, latitude DOUBLE PRECISION DEFAULT NULL, longitude DOUBLE PRECISION DEFAULT NULL, is_online TINYINT(1) DEFAULT 0 NOT NULL, online_link VARCHAR(500) DEFAULT NULL, max_capacity INT DEFAULT 0 NOT NULL, cover_image VARCHAR(500) DEFAULT NULL, status ENUM(\'UPCOMING\',\'ONGOING\',\'COMPLETED\',\'CANCELLED\'), created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, organizer_id INT NOT NULL, INDEX IDX_3BAE0AA7876C4DDA (organizer_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE event_comment (id INT AUTO_INCREMENT NOT NULL, content LONGTEXT NOT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, event_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_1123FBC371F7E88B (event_id), INDEX IDX_1123FBC3A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE event_feedback (id INT AUTO_INCREMENT NOT NULL, rating INT NOT NULL, comment LONGTEXT DEFAULT NULL, would_recommend TINYINT(1) DEFAULT 1 NOT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, participation_id INT NOT NULL, UNIQUE INDEX UNIQ_94C5AD886ACE3B73 (participation_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE event_like (id INT AUTO_INCREMENT NOT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, event_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_B3A80C1871F7E88B (event_id), INDEX IDX_B3A80C18A76ED395 (user_id), UNIQUE INDEX unique_like (event_id, user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE event_participation (id INT AUTO_INCREMENT NOT NULL, status ENUM(\'CONFIRMED\',\'PENDING\',\'CANCELLED\',\'ATTENDED\'), registered_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, qr_code VARCHAR(500) DEFAULT NULL, event_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_8F0C52E371F7E88B (event_id), INDEX IDX_8F0C52E3A76ED395 (user_id), UNIQUE INDEX unique_participation (event_id, user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
-        $this->addSql('CREATE TABLE formation (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, niveau VARCHAR(100) DEFAULT NULL, duree INT DEFAULT NULL, date_debut DATE DEFAULT NULL, date_fin DATE DEFAULT NULL, recruiter_id INT DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE formation (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, niveau VARCHAR(100) DEFAULT NULL, duree INT DEFAULT NULL, date_debut DATE DEFAULT NULL, date_fin DATE DEFAULT NULL, recruiter_id INT DEFAULT NULL, INDEX IDX_404021BF156BE243 (recruiter_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE interviews (id INT AUTO_INCREMENT NOT NULL, interview_date DATETIME NOT NULL, status VARCHAR(50) DEFAULT \'SCHEDULED\' NOT NULL, notes LONGTEXT DEFAULT NULL, location VARCHAR(100) DEFAULT NULL, meeting_link VARCHAR(500) DEFAULT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, application_id INT NOT NULL, INDEX IDX_3A7526823E030ACD (application_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE meets (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(150) NOT NULL, meet_date DATETIME NOT NULL, notes LONGTEXT DEFAULT NULL, grade DOUBLE PRECISION DEFAULT NULL, room_id VARCHAR(50) NOT NULL, meet_type VARCHAR(50) DEFAULT \'GENERAL\' NOT NULL, interview_id INT NOT NULL, UNIQUE INDEX UNIQ_673BD66F54177093 (room_id), INDEX IDX_673BD66F55D69D95 (interview_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE notifications (id INT AUTO_INCREMENT NOT NULL, type VARCHAR(50) NOT NULL, title VARCHAR(255) NOT NULL, message LONGTEXT DEFAULT NULL, link VARCHAR(500) DEFAULT NULL, is_read TINYINT(1) DEFAULT 0 NOT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, user_id INT NOT NULL, INDEX IDX_6000B0D3A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
-        $this->addSql('CREATE TABLE offers (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, department VARCHAR(100) DEFAULT NULL, contract_type VARCHAR(50) DEFAULT NULL, experience_level VARCHAR(50) DEFAULT NULL, salary_min DOUBLE PRECISION DEFAULT NULL, salary_max DOUBLE PRECISION DEFAULT NULL, location VARCHAR(150) DEFAULT NULL, status VARCHAR(50) DEFAULT NULL, publish_date DATE DEFAULT NULL, closing_date DATE DEFAULT NULL, positions_available INT DEFAULT 1 NOT NULL, applications_received INT DEFAULT 0 NOT NULL, recruiter_id INT NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE offers (id INT AUTO_INCREMENT NOT NULL, title VARCHAR(255) NOT NULL, description LONGTEXT DEFAULT NULL, department VARCHAR(100) DEFAULT NULL, contract_type VARCHAR(50) DEFAULT NULL, experience_level VARCHAR(50) DEFAULT NULL, salary_min DOUBLE PRECISION DEFAULT NULL, salary_max DOUBLE PRECISION DEFAULT NULL, location VARCHAR(150) DEFAULT NULL, status VARCHAR(50) DEFAULT NULL, publish_date DATE DEFAULT NULL, closing_date DATE DEFAULT NULL, positions_available INT DEFAULT 1 NOT NULL, applications_received INT DEFAULT 0 NOT NULL, workflow JSON DEFAULT NULL, recruiter_id INT DEFAULT NULL, INDEX IDX_DA460427156BE243 (recruiter_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE profiles (id INT AUTO_INCREMENT NOT NULL, first_name VARCHAR(50) DEFAULT NULL, last_name VARCHAR(50) DEFAULT NULL, birth_date DATE DEFAULT NULL, phone_number VARCHAR(20) DEFAULT NULL, location VARCHAR(100) DEFAULT NULL, professional_title VARCHAR(100) DEFAULT NULL, years_of_experience INT DEFAULT NULL, summary LONGTEXT DEFAULT NULL, profile_completed TINYINT(1) NOT NULL, profile_picture_path VARCHAR(500) DEFAULT NULL, cv_path VARCHAR(500) DEFAULT NULL, user_id INT DEFAULT NULL, UNIQUE INDEX UNIQ_8B308530A76ED395 (user_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
-        $this->addSql('CREATE TABLE project (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, description LONGTEXT DEFAULT NULL, status ENUM(\'PLANNED\',\'IN_PROGRESS\',\'DONE\',\'ON_HOLD\'), start_date DATE DEFAULT NULL, end_date DATE DEFAULT NULL, budget NUMERIC(10, 2) DEFAULT NULL, project_manager_id INT DEFAULT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
+        $this->addSql('CREATE TABLE project (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(100) NOT NULL, description LONGTEXT DEFAULT NULL, status ENUM(\'PLANNED\',\'IN_PROGRESS\',\'DONE\',\'ON_HOLD\'), start_date DATE DEFAULT NULL, end_date DATE DEFAULT NULL, budget NUMERIC(10, 2) DEFAULT NULL, created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, project_manager_id INT DEFAULT NULL, INDEX IDX_2FB3D0EE60984F51 (project_manager_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE question (id INT AUTO_INCREMENT NOT NULL, enonce VARCHAR(500) NOT NULL, quiz_id INT NOT NULL, INDEX IDX_B6F7494E853CD175 (quiz_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE quiz (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(255) NOT NULL, duree INT DEFAULT NULL, formation_id INT NOT NULL, INDEX IDX_A412FA925200282E (formation_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
         $this->addSql('CREATE TABLE seance (id INT AUTO_INCREMENT NOT NULL, titre VARCHAR(150) NOT NULL, type ENUM(\'PRESENTIEL\',\'EN_LIGNE\'), date_debut DATETIME NOT NULL, date_fin DATETIME NOT NULL, adresse VARCHAR(255) DEFAULT NULL, latitude DOUBLE PRECISION DEFAULT NULL, longitude DOUBLE PRECISION DEFAULT NULL, video_path VARCHAR(255) DEFAULT NULL, duree_minutes INT DEFAULT NULL, statut ENUM(\'PLANIFIEE\',\'EN_COURS\',\'TERMINEE\'), created_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL, formation_id INT NOT NULL, INDEX IDX_DF7DFD0E5200282E (formation_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4');
@@ -46,20 +48,25 @@ final class Version20260411172039 extends AbstractMigration
         $this->addSql('ALTER TABLE activities ADD CONSTRAINT FK_B5F1AFE58C03F15C FOREIGN KEY (employee_id) REFERENCES users (id)');
         $this->addSql('ALTER TABLE activities ADD CONSTRAINT FK_B5F1AFE5166D1F9C FOREIGN KEY (project_id) REFERENCES project (id)');
         $this->addSql('ALTER TABLE applications ADD CONSTRAINT FK_F7C966F0A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
-        $this->addSql('ALTER TABLE applications ADD CONSTRAINT FK_F7C966F053C674EE FOREIGN KEY (offer_id) REFERENCES offers (id)');
+        $this->addSql('ALTER TABLE applications ADD CONSTRAINT FK_F7C966F053C674EE FOREIGN KEY (offer_id) REFERENCES offers (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE bookmarks ADD CONSTRAINT FK_78D2C140A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
-        $this->addSql('ALTER TABLE bookmarks ADD CONSTRAINT FK_78D2C14053C674EE FOREIGN KEY (offer_id) REFERENCES offers (id)');
+        $this->addSql('ALTER TABLE bookmarks ADD CONSTRAINT FK_78D2C14053C674EE FOREIGN KEY (offer_id) REFERENCES offers (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE choix ADD CONSTRAINT FK_4F4880911E27F6BF FOREIGN KEY (question_id) REFERENCES question (id)');
         $this->addSql('ALTER TABLE event ADD CONSTRAINT FK_3BAE0AA7876C4DDA FOREIGN KEY (organizer_id) REFERENCES users (id)');
         $this->addSql('ALTER TABLE event_comment ADD CONSTRAINT FK_1123FBC371F7E88B FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE event_comment ADD CONSTRAINT FK_1123FBC3A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
+        $this->addSql('ALTER TABLE event_feedback ADD CONSTRAINT FK_94C5AD886ACE3B73 FOREIGN KEY (participation_id) REFERENCES event_participation (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE event_like ADD CONSTRAINT FK_B3A80C1871F7E88B FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE event_like ADD CONSTRAINT FK_B3A80C18A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
         $this->addSql('ALTER TABLE event_participation ADD CONSTRAINT FK_8F0C52E371F7E88B FOREIGN KEY (event_id) REFERENCES event (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE event_participation ADD CONSTRAINT FK_8F0C52E3A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
-        $this->addSql('ALTER TABLE interviews ADD CONSTRAINT FK_3A7526823E030ACD FOREIGN KEY (application_id) REFERENCES applications (id)');
+        $this->addSql('ALTER TABLE formation ADD CONSTRAINT FK_404021BF156BE243 FOREIGN KEY (recruiter_id) REFERENCES users (id)');
+        $this->addSql('ALTER TABLE interviews ADD CONSTRAINT FK_3A7526823E030ACD FOREIGN KEY (application_id) REFERENCES applications (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE meets ADD CONSTRAINT FK_673BD66F55D69D95 FOREIGN KEY (interview_id) REFERENCES interviews (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE notifications ADD CONSTRAINT FK_6000B0D3A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
+        $this->addSql('ALTER TABLE offers ADD CONSTRAINT FK_DA460427156BE243 FOREIGN KEY (recruiter_id) REFERENCES users (id)');
         $this->addSql('ALTER TABLE profiles ADD CONSTRAINT FK_8B308530A76ED395 FOREIGN KEY (user_id) REFERENCES users (id)');
+        $this->addSql('ALTER TABLE project ADD CONSTRAINT FK_2FB3D0EE60984F51 FOREIGN KEY (project_manager_id) REFERENCES users (id)');
         $this->addSql('ALTER TABLE question ADD CONSTRAINT FK_B6F7494E853CD175 FOREIGN KEY (quiz_id) REFERENCES quiz (id)');
         $this->addSql('ALTER TABLE quiz ADD CONSTRAINT FK_A412FA925200282E FOREIGN KEY (formation_id) REFERENCES formation (id)');
         $this->addSql('ALTER TABLE seance ADD CONSTRAINT FK_DF7DFD0E5200282E FOREIGN KEY (formation_id) REFERENCES formation (id)');
@@ -85,13 +92,18 @@ final class Version20260411172039 extends AbstractMigration
         $this->addSql('ALTER TABLE event DROP FOREIGN KEY FK_3BAE0AA7876C4DDA');
         $this->addSql('ALTER TABLE event_comment DROP FOREIGN KEY FK_1123FBC371F7E88B');
         $this->addSql('ALTER TABLE event_comment DROP FOREIGN KEY FK_1123FBC3A76ED395');
+        $this->addSql('ALTER TABLE event_feedback DROP FOREIGN KEY FK_94C5AD886ACE3B73');
         $this->addSql('ALTER TABLE event_like DROP FOREIGN KEY FK_B3A80C1871F7E88B');
         $this->addSql('ALTER TABLE event_like DROP FOREIGN KEY FK_B3A80C18A76ED395');
         $this->addSql('ALTER TABLE event_participation DROP FOREIGN KEY FK_8F0C52E371F7E88B');
         $this->addSql('ALTER TABLE event_participation DROP FOREIGN KEY FK_8F0C52E3A76ED395');
+        $this->addSql('ALTER TABLE formation DROP FOREIGN KEY FK_404021BF156BE243');
         $this->addSql('ALTER TABLE interviews DROP FOREIGN KEY FK_3A7526823E030ACD');
+        $this->addSql('ALTER TABLE meets DROP FOREIGN KEY FK_673BD66F55D69D95');
         $this->addSql('ALTER TABLE notifications DROP FOREIGN KEY FK_6000B0D3A76ED395');
+        $this->addSql('ALTER TABLE offers DROP FOREIGN KEY FK_DA460427156BE243');
         $this->addSql('ALTER TABLE profiles DROP FOREIGN KEY FK_8B308530A76ED395');
+        $this->addSql('ALTER TABLE project DROP FOREIGN KEY FK_2FB3D0EE60984F51');
         $this->addSql('ALTER TABLE question DROP FOREIGN KEY FK_B6F7494E853CD175');
         $this->addSql('ALTER TABLE quiz DROP FOREIGN KEY FK_A412FA925200282E');
         $this->addSql('ALTER TABLE seance DROP FOREIGN KEY FK_DF7DFD0E5200282E');
@@ -108,10 +120,12 @@ final class Version20260411172039 extends AbstractMigration
         $this->addSql('DROP TABLE choix');
         $this->addSql('DROP TABLE event');
         $this->addSql('DROP TABLE event_comment');
+        $this->addSql('DROP TABLE event_feedback');
         $this->addSql('DROP TABLE event_like');
         $this->addSql('DROP TABLE event_participation');
         $this->addSql('DROP TABLE formation');
         $this->addSql('DROP TABLE interviews');
+        $this->addSql('DROP TABLE meets');
         $this->addSql('DROP TABLE notifications');
         $this->addSql('DROP TABLE offers');
         $this->addSql('DROP TABLE profiles');
